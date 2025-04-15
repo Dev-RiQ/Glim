7332b69a15a03b5f5b9666ccbee05532f89ea91e
Dev-RiQ <lwk525678@gmail.com>
2025-04-15 12:49:15 +0900

    UPDATE : Project Name

--- src/main/java/com/glim/GlimApplication.java
+++ src/main/java/com/glim/GlimApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RestInApplication {
+public class GlimApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(RestInApplication.class, args);
+        SpringApplication.run(GlimApplication.class, args);
     }
 
 }
