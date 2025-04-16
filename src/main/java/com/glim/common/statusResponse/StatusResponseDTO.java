ea02310c6cf4f06268fdc93bd8efcdf442af1505
Dev-RiQ <lwk525678@gmail.com>
2025-04-16 23:53:53 +0900

    INSERT : StatusResponseDTO

---
+++ src/main/java/com/glim/common/statusResponse/StatusResponseDTO.java
@@ -0,0 +1,42 @@
+package com.glim.common.statusResponse;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class StatusResponseDTO {
+    private final Integer status = 200;
+    private final String message = "OK";
+    private Object data;
+
+    public static StatusResponseDTO ok() {
+        return new StatusResponseDTO();
+    }
+
+    public static StatusResponseDTO ok(Object data) {
+        return new StatusResponseDTO(data);
+    }
+
+    public static StatusResponseDTO ok(List<Object> data) {
+        return new StatusResponseDTO(data);
+    }
+
+    public static StatusResponseDTO ok(Map<Object,Object> data) {
+        return new StatusResponseDTO(data);
+    }
+
+    public static StatusResponseDTO ok(Set<Object> data) {
+        return new StatusResponseDTO(data);
+    }
+}
